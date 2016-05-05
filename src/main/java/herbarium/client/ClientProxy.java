9058aeab4b632cd8f267b86668026ad7d4926854
s0cks <tasman.vose@gmail.com>
2016-05-05 18:15:05 +0000

    Finished Page renderer?

--- src/main/java/herbarium/client/ClientProxy.java
+++ src/main/java/herbarium/client/ClientProxy.java
@@ -1,5 +1,6 @@
 package herbarium.client;
 
+import herbarium.client.render.RenderItemPageFP;
 import herbarium.common.CommonProxy;
 import herbarium.common.Herbarium;
 import net.minecraft.block.Block;
@@ -7,6 +8,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.FMLClientHandler;
 
 public final class ClientProxy
@@ -39,6 +41,8 @@ extends CommonProxy{
         registerRender(Herbarium.blockCrucible, "crucible");
         registerRender(Herbarium.blockFlume, "flume");
         registerRender(Herbarium.blockPipe, "pipe");
+
+        MinecraftForge.EVENT_BUS.register(new RenderItemPageFP());
     }
 
     private void registerRender(Block block, String id){
--- src/main/java/herbarium/client/render/RenderItemPageFP.java
+++ src/main/java/herbarium/client/render/RenderItemPageFP.java
@@ -11,8 +11,9 @@ import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.VertexBuffer;
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.client.event.RenderHandEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.lwjgl.opengl.GL11;
@@ -20,73 +21,21 @@ import org.lwjgl.opengl.GL11;
 public final class RenderItemPageFP{
     private static final ResourceLocation texture = new ResourceLocation("herbarium", "textures/gui/note_gui.png");
 
-    /*
     @SubscribeEvent
     public void doRender(RenderHandEvent e){
         Minecraft mc = this.client();
-        if(mc.thePlayer.getHeldItem() == null) return;
-        if(mc.thePlayer.getHeldItem().getItem() instanceof ItemPage){
-
-            /*
-            GL11.glTranslatef(1.0F, 0.75F, -1.0F);
-      GL11.glRotatef(135.0F, 0.0F, -1.0F, 0.0F);
-
-      float f3 = playersp.prevRenderArmPitch + (playersp.renderArmPitch - playersp.prevRenderArmPitch) * par1;
-      float f4 = playersp.prevRenderArmYaw + (playersp.renderArmYaw - playersp.prevRenderArmYaw) * par1;
-      GL11.glRotatef((playermp.field_70125_A - f3) * 0.1F, 1.0F, 0.0F, 0.0F);
-      GL11.glRotatef((playermp.field_70177_z - f4) * 0.1F, 0.0F, 1.0F, 0.0F);
-
-      float var4 = playermp.field_70127_C + (playermp.field_70125_A - playermp.field_70127_C) * par1;
-
-      float f1 = UtilsFX.getPrevEquippedProgress(mc.entityRenderer.itemRenderer) + (UtilsFX.getEquippedProgress(mc.entityRenderer.itemRenderer) - UtilsFX.getPrevEquippedProgress(mc.entityRenderer.itemRenderer)) * par1;
-
-      GL11.glTranslatef(-0.7F * var7, -(-0.65F * var7) + (1.0F - f1) * 1.5F, 0.9F * var7);
-
-      GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
-
-      GL11.glTranslatef(0.0F, 0.0F * var7, -0.9F * var7);
-      GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
-      GL11.glRotatef(0.0F, 0.0F, 0.0F, 1.0F);
-      GL11.glEnable(32826);
-
-      GL11.glPushMatrix();
-      GL11.glScalef(5.0F, 5.0F, 5.0F);
-
-      mc.renderEngine.bindTexture(mc.thePlayer.func_110306_p());
-      for (int var9 = 0; var9 < 2; var9++)
-      {
-        int var22 = var9 * 2 - 1;
-        GL11.glPushMatrix();
-        GL11.glTranslatef(-0.0F, -0.6F, 1.1F * var22);
-        GL11.glRotatef(-45 * var22, 1.0F, 0.0F, 0.0F);
-        GL11.glRotatef(-90.0F, 0.0F, 0.0F, 1.0F);
-        GL11.glRotatef(59.0F, 0.0F, 0.0F, 1.0F);
-        GL11.glRotatef(-65 * var22, 0.0F, 1.0F, 0.0F);
-        Render var24 = RenderManager.instance.getEntityRenderObject(mc.thePlayer);
-        RenderPlayer var26 = (RenderPlayer)var24;
-        float var13 = 1.0F;
-        GL11.glScalef(var13, var13, var13);
-        var26.renderFirstPersonArm(mc.thePlayer);
-        GL11.glPopMatrix();
-      }
-      GL11.glPopMatrix();
-      GL11.glRotatef(90.0F, 0.0F, 0.0F, 1.0F);
-
-      GL11.glTranslatef(0.4F, -0.4F, 0.0F);
-      GL11.glEnable(32826);
-      GL11.glScalef(2.0F, 2.0F, 2.0F);
-
-
+        if(mc.thePlayer.getHeldItem(EnumHand.MAIN_HAND) == null) return;
+        if(mc.thePlayer.getHeldItem(EnumHand.MAIN_HAND).getItem() instanceof ItemPage && mc.thePlayer.getHeldItemOffhand() == null && mc.gameSettings.thirdPersonView == 0){
             e.setCanceled(true);
 
             Tessellator tess = Tessellator.getInstance();
-            VertexBuffer rend = tess.getWorldRenderer();
+            VertexBuffer rend = tess.getBuffer();
             EntityPlayerSP p = mc.thePlayer;
 
-            float pitch = p.prevRotationPitch + (p.rotationPitch - p.prevRotationPitch) * e.partialTicks;
-            float yaw = p.prevRotationYaw + (p.rotationYaw - p.prevRotationYaw) * e.partialTicks;
+            float pitch = p.prevRotationPitch + (p.rotationPitch - p.prevRotationPitch) * e.getPartialTicks();
+            float yaw = p.prevRotationYaw + (p.rotationYaw - p.prevRotationYaw) * e.getPartialTicks();
             this.rotate(pitch, yaw);
-            this.rotatePitch(p, e.partialTicks);
+            this.rotatePitch(p, e.getPartialTicks());
 
             GlStateManager.enableRescaleNormal();
             GlStateManager.pushMatrix();
@@ -96,13 +45,11 @@ public final class RenderItemPageFP{
 
             GlStateManager.rotate(4.0F, 0.0F, 1.0F, 0.0F);
             GlStateManager.translate(0.0F, 1.6F, 0.0F);
-            float f3 = cos(pitch);
             GlStateManager.translate(0.0F, 0.04F, -0.72F);
             GlStateManager.translate(0.0F, -0.3F, 0.0F);
-            GlStateManager.translate(0.0F, f3 * -0.5F, 0.0F);
-            GlStateManager.rotate(90.0F, 0.0F, 1.0F, 0.0F);
-            GlStateManager.rotate(f3 * -85.0F, 0.0F, 0.0F, 1.0F);
-            GlStateManager.rotate(0.0F, 1.0F, 0.0F, 0.0F);
+            GlStateManager.rotate(87.0F, 0.0F, 1.0F, 0.0F);
+            GlStateManager.translate(0.0F, -1.0F, 0.0F);
+            GlStateManager.translate(0.1F, 0.0F, 0.0F);
 
             GlStateManager.disableCull();
             this.renderLeftArm(p, render);
@@ -114,8 +61,8 @@ public final class RenderItemPageFP{
             GlStateManager.rotate(180.0F, 0.0F, 0.0F, 1.0F);
             GlStateManager.rotate(0.0F, 1.0F, 0.0F, 0.0F);
             GlStateManager.translate(-1.0F, -0.05F, -1.0F);
+            GlStateManager.translate(0.0F, 0.0F, 0.5F);
             GlStateManager.scale(0.03F, 0.03F, 0.03F);
-            GlStateManager.rotate(30.0F, 1.0F, 0.0F, 0.0F);
             mc.getTextureManager().bindTexture(texture);
             GL11.glNormal3f(0.0F, 0.0F, -1.0F);
 
@@ -134,7 +81,7 @@ public final class RenderItemPageFP{
                     .endVertex();
             tess.draw();
 
-            IPage page = ItemPage.getPage(p.getHeldItem());
+            IPage page = ItemPage.getPage(p.getHeldItem(EnumHand.MAIN_HAND));
             if(page != null){
                 GlStateManager.scale(0.5F, 0.5F, 0.5F);
                 GlStateManager.translate(-5.0F, 10.0F, -1.0F);
@@ -194,5 +141,4 @@ public final class RenderItemPageFP{
     private void renderPage(){
 
     }
-    */
 }
