44c8bf4eb206dd100f8e1f17fee15c2352e1c17f
Yulife <YulifeGaming@t-online.de>
2016-05-06 14:15:41 +0200

    Water Flower

---
+++ src/main/java/herbarium/common/blocks/BlockWaterFlower.java
@@ -0,0 +1,20 @@
+package herbarium.common.blocks;
+
+import herbarium.common.core.Flowers;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public final class BlockWaterFlower
+        extends BlockHerbariumFlower{
+    public BlockWaterFlower(){
+        super(Flowers.SPRING_LOTUS);
+    }
+
+    @Override
+    public boolean canPlaceBlockAt(World worldIn, BlockPos pos) {
+        Block top = worldIn.getBlockState(pos.up()).getBlock();
+        return top == Blocks.WATERLILY;
+    }
+}
